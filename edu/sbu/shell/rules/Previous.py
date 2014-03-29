129ff707e9a906ac0fc752e20d25e5997f3b494a
gthandavam <gthandavam@gmail.com>
2014-03-29 18:45:23 -0400

    saving

--- edu/sbu/shell/rules/Previous.py
+++ edu/sbu/shell/rules/Previous.py
@@ -6,11 +6,28 @@ class Previous:
   def __init__(self):
     pass
 
+
+  def find_previous(self, pnodes, find_i, find_j):
+    prev_i = -1
+    prev_j = -1
+    #brute forcing for now - optmize later
+    for i in xrange(len(pnodes)):
+      for j in xrange(len(pnodes[i])):
+        if i == find_i and j == find_j:
+          break
+
+        if not pnodes[i][j] is None:
+          prev_i = i
+          prev_j = j
+
+    return prev_i, prev_j
+
   def run(self, pnodes, rnodes):
     for i in xrange(1, len(rnodes)):
       for j in xrange(len(rnodes[i])):
-        prev_j = j-1 if j != 0 else len(rnodes[i-1]) - 1
-        prev_i = i if j!= 0 else i-1
+
+        prev_i, prev_j = self.find_previous(pnodes, i, j)
+          # i if j!= 0 else i-1
         for k in xrange(3):
           if not rnodes[i][j][k] is None:
             rnodes[i][j][k].shell_coref.append((prev_i, prev_j))
