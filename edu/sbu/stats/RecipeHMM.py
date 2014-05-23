c477a315766605dfa62f55e169ceb0c14c98191e
gthandavam <gthandavam@gmail.com>
2014-05-23 02:09:29 -0400

    adding class for HMM formulation

---
+++ edu/sbu/stats/RecipeHMM.py
@@ -0,0 +1,22 @@
+__author__ = 'gt'
+
+class RecipeHMM:
+
+  def calc_words_bigram(self):
+    pass
+
+  def calc_verbs_bigram(self):
+    pass
+
+  def calc_verbs_unigram(self):
+    pass
+
+  def calc_phrase_probability(self, arg_type):
+    pass
+
+  def conditional_prob(self, arg_type):
+    pass
+
+
+  pass
+
