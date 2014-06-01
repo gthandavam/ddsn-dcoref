7a074abc51db3200f5b090f2f20d942a4a5ff855
gthandavam <gthandavam@gmail.com>
2014-05-31 23:00:10 -0400

    highlighting the edges that the users should focus on

--- edu/sbu/eval/user_eval/generate_eval_graphs.py
+++ edu/sbu/eval/user_eval/generate_eval_graphs.py
@@ -1,6 +1,6 @@
 __author__ = 'gt'
 
-from edu.sbu.eval.user_eval.diff_edges import  intersect_edges, diff_edges
+from edu.sbu.eval.user_eval.diff_edges import  common_graph_lines, diff_edges
 
 import os
 testArchive = '/home/gt/Documents/UserEvaluation/'
@@ -11,17 +11,42 @@ if __name__ == '__main__':
 
   #algoA and algoB should have same length!!!
   for i in xrange(len(algoA)):
-    with open(algoA[i]) as f:
+    A_file = testArchive + '/AlgoA/' + algoA[i]
+    B_file = testArchive + '/AlgoB/' + algoB[i]
+    with open(A_file) as f:
       a_lines = f.readlines()
 
-    with open(algoB[i]) as f:
+    with open(B_file) as f:
       b_lines = f.readlines()
 
     a_diff_b, b_diff_a = diff_edges(a_lines, b_lines)
-    
-
+    common = common_graph_lines(a_lines, b_lines)
+    a_out = A_file.replace('AlgoA', 'AlgoA_diff')
+    b_out = B_file.replace('AlgoB', 'AlgoB_diff')
     pass
 
+    with open(a_out, 'w') as f:
+      f.write('Digraph G {\n')
+      for line in common:
+        if line.find('};') != -1 or line.find('Digraph') != -1:
+          continue
+        f.write(line)
 
+      for line in a_diff_b:
+        f.write(line.rstrip() + '[color=red]\n')
+
+      f.write('};')
+
+    with open(b_out, 'w') as f:
+      f.write('Digraph G {\n')
+      for line in common:
+        if line.find('};') != -1 or line.find('Digraph') != -1:
+          continue
+        f.write(line)
+
+      for line in b_diff_a:
+        f.write(line.rstrip() + '[color=red]\n')
+
+      f.write('};')
 
   pass
