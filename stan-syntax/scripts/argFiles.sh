ab6cb1cc23074c94553b747811d5f238abc1b89d
gthandavam <gthandavam@gmail.com>
2014-08-28 14:28:36 -0400

    Saving the scripts used for pre-processing recipe text

---
+++ stan-syntax/scripts/argFiles.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+modelDir=/home/gt/Downloads/stanford-corenlp-full-2014-01-04/
+classpath=/home/gt/eclipse_workspace/dcoref-Jan4-14/bin/
+recipeName=$1
+
+java -Djava.ext.dirs=$modelDir  -cp $classpath  edu.sbu.recipe.preprocess.PrefixI  $recipeName
+java -Djava.ext.dirs=$modelDir  -cp $classpath  edu.sbu.recipe.trees.PrintParseTrees  $recipeName
+java -Djava.ext.dirs=$modelDir  -cp $classpath  edu.sbu.recipe.args.RecipeArgs  $recipeName
